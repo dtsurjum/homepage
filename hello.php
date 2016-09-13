f46cd57c03af433cb7122c581b277cde10367b2d
Dmitri Tšurjumov <dtsurjum@klass1-14-l.intra.itcollege.ee>
2016-09-13 17:35:43 +0300

    ignore index.xtml

---
+++ hello.php
@@ -0,0 +1,16 @@
+The rest is actually HTML
+<?php
+
+
+# var name = prompt
+# alert ("hello" + name)
+# This is hello world in PHP
+# This file is saved to public_html as hello.php
+
+echo "Hello world";
+echo "Today is " . date("l");
+
+# Run open
+# http://enos.itcollege.ee/~dtsurjum/hello.php
+
+phpinfo();
